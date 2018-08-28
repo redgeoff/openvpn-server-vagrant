00c7284f264a4b27a4bf6d019d0471a58e52d06a
Geoff Cox <redgeoff@gmail.com>
2018-08-28 06:11:16 -0700

    Revert "Reconfigured to use build-key-pass instead of build-key"

--- build-key.sh
+++ build-key.sh
@@ -7,25 +7,7 @@
 
 set name [lindex $argv 0];
 
-# Get the password for the VPN account
-stty -echo
-send_user -- "VPN User Password: "
-expect_user -re "(.*)\n"
-send_user "\n"
-stty echo
-set pass $expect_out(1,string)
-
-# Confirm the password for the VPN account
-stty -echo
-send_user -- "Confirm VPN User Password: "
-expect_user -re "(.*)\n"
-send_user "\n"
-stty echo
-set confirm_pass $expect_out(1,string)
-
-spawn ./build-key-pass $name
-send -- "$pass\r"
-send -- "$confirm_pass\r"
+spawn ./build-key $name
 expect "Country Name"
 send "\n"
 expect "State or Province Name"
